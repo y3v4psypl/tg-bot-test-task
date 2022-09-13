97ee7162ba247fe6b377858790a2faf174399bd7
Eva Vetrova <yevavetrova@gmail.com>
2022-09-13 14:03:24 +0300

    WIP

--- index.ts
+++ index.ts
@@ -10,8 +10,9 @@ config();
 let TOKEN = process.env.TELEGRAM_API_TOKEN || 'undefined';
 
 // postgres
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const client = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: connectionString,
     ssl: {
     rejectUnauthorized: false
     }
