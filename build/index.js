3da2b36eb17556357e129e1c88e17af21d3cab64
Eva Vetrova <yevavetrova@gmail.com>
2022-09-13 13:00:57 +0300

    WIP

--- build/index.js
+++ build/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,6 +38,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 const node_telegram_bot_api_1 = __importDefault(require("node-telegram-bot-api"));
 const axios_1 = __importDefault(require("axios"));
+const fs = __importStar(require("fs"));
 // access env variables
 (0, dotenv_1.config)();
 let TOKEN = process.env.TELEGRAM_API_TOKEN || 'undefined';
@@ -31,10 +55,11 @@ bot.onText(/\/start/gm, (msg) => {
 bot.onText(/\/wannaread/gm, (msg) => {
     const photo = 'https://pythonist.ru/wp-content/uploads/2020/03/photo_2021-02-03_10-47-04-350x2000-1.jpg';
     const caption = 'Идеальный карманный справочник для быстрого ознакомления с особенностями работы разработчиков на Python. Вы найдете море краткой информации о типах и операторах в Python, именах специальных методов, встроенных функциях, исключениях и других часто используемых стандартных модулях.';
+    const file = fs.createReadStream('../files/python-book.zip');
     bot.sendPhoto(msg.chat.id, photo, {
         caption,
     }).catch(e => console.log(e));
-    bot.sendDocument(msg.chat.id, 'python-book.zip').catch(e => console.log(e));
+    bot.sendDocument(msg.chat.id, file).catch(e => console.log(e));
 });
 bot.onText(/\/weather/gm, (msg) => __awaiter(void 0, void 0, void 0, function* () {
     const currentTime = new Date().toISOString().slice(0, -10).concat('00');
