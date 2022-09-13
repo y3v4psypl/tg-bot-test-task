334dbe231dc5597525793948e00a9b329895ef8d
Eva <yevavetrova@gmail.com>
2022-09-14 01:34:52 +0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -29,11 +29,8 @@ const pool = new pg.Pool({
 const bot = new TelegramBot(TOKEN, { polling: true });
 if (bot) { console.log('Bot is running') }
 
-let message: Message;
-
 bot.onText(/\/start/gm, async (msg: Message) => {
-    message = msg;
-    await bot.sendMessage(Number(msg.from?.id), 'Здравствуйте. Нажмите на любую интересующую Вас кнопку', {
+    await bot.sendMessage(msg.chat.id, 'Здравствуйте. Нажмите на любую интересующую Вас кнопку', {
         "reply_markup": {
             "inline_keyboard": [
                 [
@@ -65,30 +62,25 @@ bot.onText(/\/start/gm, async (msg: Message) => {
 
 bot.on('callback_query', async (callback_query) => {
     const action = callback_query.data;
-    console.log(callback_query.data, callback_query.id)
+    const chatId = callback_query.from.id;
 
     switch (action) {
         case StartActionType.GetWeather:
-            if (message.from?.id != null) {
-                await bot.answerCallbackQuery(callback_query.id)
-                await bot.sendMessage(message.from?.id, `Сейчас в Оттаве (Канада) ${await getWeather()}°C`);
-            }
+            await bot.answerCallbackQuery(callback_query.id);
+            await bot.sendMessage(chatId, `Сейчас в Оттаве (Канада) ${await getWeather()}°C`);
         break;
         case StartActionType.PythonHandbook:
             const photo = 'https://pythonist.ru/wp-content/uploads/2020/03/photo_2021-02-03_10-47-04-350x2000-1.jpg';
             const caption = 'Идеальный карманный справочник для быстрого ознакомления с особенностями работы разработчиков на Python. Вы найдете море краткой информации о типах и операторах в Python, именах специальных методов, встроенных функциях, исключениях и других часто используемых стандартных модулях.';
             const file = fs.createReadStream('files/python-book.zip');
 
-            if (message.from?.id != null) {
-                await bot.answerCallbackQuery(callback_query.id)
-                await bot.sendPhoto(message.from.id, photo, {caption});
-                await bot.sendDocument(message.from.id, file).catch(e => console.log(e));
-                console.log('File has been sent');
-            }
+            await bot.answerCallbackQuery(callback_query.id)
+            await bot.sendPhoto(chatId, photo, {caption});
+            await bot.sendDocument(chatId, file);
         break;
         case StartActionType.Broadcasting:
             await bot.answerCallbackQuery(callback_query.id)
-            await bot.sendMessage(Number(message.from?.id), 'Вы выбрали рассылку всем пользователям. Вы уверены что хотите это сделать?', {
+            await bot.sendMessage(chatId, 'Вы выбрали рассылку всем пользователям. Вы уверены что хотите это сделать?', {
                 "reply_markup": {
                     "inline_keyboard": [
                         [
@@ -106,35 +98,33 @@ bot.on('callback_query', async (callback_query) => {
 
 bot.on('callback_query', async (callback_query) => {
     const action = callback_query.data;
+    const chatId = callback_query.from.id;
     switch (action) {
         case BroadcastingActionType.Yes:
             await bot.answerCallbackQuery(callback_query.id)
 
-            const messageBroadcat = await bot.sendMessage(Number(message.from?.id), 'Введите сообщение, которое хотите отправить всем пользователям.', {
+            const messageBroadcat = await bot.sendMessage(chatId, 'Введите сообщение, которое хотите отправить всем пользователям.', {
                 reply_markup: {
                     force_reply: true,
                 },
             });
-            bot.onReplyToMessage(Number(message.from?.id), messageBroadcat.message_id, async (message) => {
+            bot.onReplyToMessage(chatId, messageBroadcat.message_id, async (message) => {
                 const res = await getAllUserIds(pool);
 
-                res.rows.forEach(chatID => {
-                    if (message.text != null && chatID[0] != Number(message.from?.id)) {
+                res.rows.forEach(id => {
+                    if (message.text != null && id[0] != chatId) {
                         bot.sendMessage(chatID[0], message.from?.username == undefined
                             ? `Анонимное сообщение: ${message.text}`
-                            : `Сообщение от ${message.from?.username} ${message.text}`);
+                            : `Сообщение от ${message.from?.username}: ${message.text}`);
                     }
                 });
-                await bot.sendMessage(Number(message.from?.id), `Вы отправили сообщение: ${message.text}`)
+                await bot.sendMessage(chatId, `Вы отправили сообщение: ${message.text}`)
             });
             break;
         case BroadcastingActionType.No:
-            await bot.sendMessage(Number(message.from?.id), 'Вы отказались от рассылки')
+            await bot.sendMessage(chatId, 'Вы отказались от рассылки')
     }
-
 });
-
-
 
 bot.on('polling_error', console.log);
 
